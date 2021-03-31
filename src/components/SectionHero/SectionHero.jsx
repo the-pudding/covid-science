6226ddbdd3ded0602090175c67a8dbdd9b665503
iblind <no@email.com>
2021-03-31 12:47:52 -0400

    socials and meta

--- src/components/SectionHero/SectionHero.jsx
+++ src/components/SectionHero/SectionHero.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import './SectionHero.scoped.scss';
 
 const SectionHero = (props) => {
-  return (
-    <div className={props.rootClassName}>
-      <div className="title-container">
-        <h1 className="title">Following the Science</h1>
-        <div className="subtitle">
-          A look at the global research effort to know and defeat COVID-19
+    return (
+        <div className={props.rootClassName}>
+            <div className="title-container">
+                <h1 className="title">Following the Science</h1>
+                <div className="subtitle">
+                    A look at the global research effort to know and defeat COVID-19
         </div>
-        <div className="byline">
-          By{' '}
-          <a href="https://jeffmacinnes.com" target="_blank">
-            Jeff MacInnes
+                <div className="byline">
+                    By{' '}
+                    <a href="https://pudding.cool/author/jeff-macinnes" target="_blank">
+                        Jeff MacInnes
           </a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default SectionHero;
